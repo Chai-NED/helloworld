bb8241536efc8f7354b89cfb079830a0d4210b17
Chai Swaddipong <chswaddi@microsoft.com>
2019-02-06 10:04:58 -0500

    Reset head

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = process.env.PORT || 1557;
+var port = process.env.PORT || 1558;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
